f37321eada79b7102f01c99af91b40f825c87c41
EC2 Default User <ec2-user@ip-172-31-16-8.us-west-2.compute.internal>
2020-02-17 01:27:56 +0000

    Added sorting to title column

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all(order_by :title)
+    @movies = Movie.all
     #@movie = Movie.order(params[:sort])
   end
 
